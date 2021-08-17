093f20cc4cecd7d338b619be5fda7a821c4042aa
G37_sG <skyriyvrzo@gmail.com>
2021-08-17 09:43:09 +0700

    ItemCreate

--- src/main/java/com/wehs/gitenraft/items/tool/Emerald_pickaxe.java
+++ src/main/java/com/wehs/gitenraft/items/tool/Emerald_pickaxe.java
@@ -1,10 +1,20 @@
 package com.wehs.gitenraft.items.tool;
 
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Material;
 
 public class Emerald_pickaxe extends PickaxeItem {
     public Emerald_pickaxe(Properties PropertPickaxe) {
         //super(Properties, attackDamage, speedAttack, Properties)
         super(ItemCreate.Emerald_Pickaxe, -1, -2.8f, PropertPickaxe);   // Base AD +6.0 AD // Base AS +4.0 AS
     }
+    
+	@Override
+	public float getDestroySpeed(ItemStack p_150893_1_, BlockState p_150893_2_) {
+		Material material = p_150893_2_.getMaterial();
+		return material != Material.METAL && material != Material.HEAVY_METAL && material != Material.STONE ? super.getDestroySpeed(p_150893_1_, p_150893_2_) : this.speed * 4;
+		
+	}
 }
