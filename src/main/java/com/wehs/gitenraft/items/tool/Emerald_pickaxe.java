554c575a6f5848314fd6e1aab59d7b422b61a5fc
G37_sG <skyriyvrzo@gmail.com>
2021-08-17 09:26:59 +0700

    Update Emerald_pickaxe.java

--- src/main/java/com/wehs/gitenraft/items/tool/Emerald_pickaxe.java
+++ src/main/java/com/wehs/gitenraft/items/tool/Emerald_pickaxe.java
@@ -5,6 +5,6 @@ import net.minecraft.world.item.PickaxeItem;
 public class Emerald_pickaxe extends PickaxeItem {
     public Emerald_pickaxe(Properties PropertPickaxe) {
         //super(Properties, attackDamage, speedAttack, Properties)
-        super(ItemCreate.Emerald_Pickaxe, -1, -2.8f, PropertPickaxe);   // Base AD +6.0 AD// Base AS +4.0 AS
+        super(ItemCreate.Emerald_Pickaxe, -1, -2.8f, PropertPickaxe);   // Base AD +6.0 AD // Base AS +4.0 AS
     }
 }
