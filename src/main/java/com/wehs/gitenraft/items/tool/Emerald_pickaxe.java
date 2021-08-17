2e4f51678edf5c01f17a5d4c12ff3eabe1388c01
G37_sG <skyriyvrzo@gmail.com>
2021-08-17 10:12:13 +0700

    2nd

--- src/main/java/com/wehs/gitenraft/items/tool/Emerald_pickaxe.java
+++ src/main/java/com/wehs/gitenraft/items/tool/Emerald_pickaxe.java
@@ -8,7 +8,7 @@ import net.minecraft.world.level.material.Material;
 public class Emerald_pickaxe extends PickaxeItem {
     public Emerald_pickaxe(Properties PropertPickaxe) {
         //super(Properties, attackDamage, speedAttack, Properties)
-        super(ItemCreate.Emerald_Pickaxe, -1, -2.8f, PropertPickaxe);   // Base AD +6.0 AD // Base AS +4.0 AS
+        super(ItemCreate.Emerald_Pickaxe, 1, -2.8f, PropertPickaxe);   // Base AD +6.0 AD // Base AS +4.0 AS
     }
     
 	@Override
